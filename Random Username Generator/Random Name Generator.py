3b0eede45f2d24ff80d53fe735366efcec4ee2e5
Hacker-Boy9999 <fordaniyal@outlook.com>
2021-05-29 14:20:43 +0300

    Create Random Name Generator.py

---
+++ Random Username Generator/Random Name Generator.py
@@ -0,0 +1,5 @@
+import random 
+
+Names = ["Tom", "Timmy", "Bob", "Sophia", "Olivia", "Riley", "Emma", "Ava", "Liam", "Noah", "Jackson", "Aiden", "Elijah", "Ahmed", "Atif", "Badr", "Basheer", "Dalil", "Dayyan", "Faiz", "Farhan", "Faysal", "Fida", "Ghani", "Ghazi", "Hamza", "Hassan", "Imad", "Iqbal", "Irfan", "Jamal", "Kafeel", "Aminah", "Aisha", "Asma", "Basimah", "Dariya", "Farhana", "Hana", "Huda", "Iman", "Jameela", "Kareema"]
+Answer = random.choice(Names)
+print(Answer)
